c96c224220f60391e4ed0230f2d0eeefbc9e518e
Dominic Garms <djgarms@gmail.com>
2015-05-01 12:15:08 +0800

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # meteor-jquery-fullcalendar
 Arshaw FullCalendar jquery plugin wrapped source for Meteor
+
+## Install
+meteor add planettraining:jquery-fullcalendar
---
+++ package.js
@@ -0,0 +1,20 @@
+Package.describe({
+  summary: "Arshaw FullCalendar jquery plugin original source.",
+  name: "planettraining:jquery-fullcalendar",
+  version: "2.3.1",
+  git:"https://github.com/planet-training/meteor-jquery-fullcalendar.git"
+});
+
+
+Package.onUse(function (api) {
+  api.versionsFrom('1.0');
+
+  api.use("jquery", "client");
+  api.use("momentjs:moment@2.0.0");
+
+  api.addFiles([
+    'bower_components/fullcalendar/dist/fullcalendar.js',
+    'bower_components/fullcalendar/dist/fullcalendar.css',
+    'bower_components/fullcalendar/dist/fullcalendar.print.css'
+  ], "client");
+});
